29f0aa77af4e1d512692f9faef494fe816429c44
praveen katewa <praveenkatewa.45@gmail.com>
2024-11-16 22:51:09 +0530

    new files

---
+++ projects/forms/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import FormWithoutYup from './components/form-without-yup'
+
+function App() {
+ 
+
+  return( 
+  <div>
+  <FormWithoutYup/>
+  </div>
+  )
+}
+
+export default App
